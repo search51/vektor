25efa3c5d8f552b3afb01e17f6a137578436ca56
Nils Honus <nils@honus.de>
2019-10-12 17:15:59 +0200

    Soweit

---
+++ ConsoleApplication1/ConsoleApplication1/Vektor.h
@@ -0,0 +1,76 @@
+#pragma once
+template <class T>
+class Vektor
+{
+private:
+  struct element {
+    struct element* last;
+    struct element* next;
+    T m_content;
+    unsigned int id;
+  };
+  struct element* head = 0;
+  struct element* tail = 0;
+  struct element* phelp = 0;
+  int m_size = 0;
+public:
+
+  Vektor() {}
+  ~Vektor() {}
+
+
+  void pushback(T value) {
+    if (head == 0) {
+      head = new struct element;
+      head->next = 0;
+      head->last = 0;
+      tail = head;
+    }
+    else {
+      head->last = new struct element;
+      head->last->last = 0;
+      head->last->next = head;
+      head = head->last;
+    }
+    head->m_content = value;
+    head->id = m_size;
+    ++m_size;
+  }
+  void popback() {
+    if (head != 0) {
+      head = head->next;
+      delete head->last;
+      head->last = 0;
+      --m_size;
+    }
+  }
+
+  T at(int pos) {
+    phelp = tail;
+    for (int i = 0; i < m_size; ++i) {
+      if (pos == phelp->id) {
+        return phelp->m_content;
+      }
+      if (i < m_size - 1) {
+        phelp = phelp->last;
+      }
+    }
+  }
+
+  int size() {
+    return m_size;
+  }
+  void resize(int size) {
+    if (size < m_size) {
+      do {
+        popback();
+      } while (size < m_size);
+    }
+    if (size > m_size) {
+      do {
+        pushback(0);
+      } while (size > m_size);
+    }
+  }
+};
+
