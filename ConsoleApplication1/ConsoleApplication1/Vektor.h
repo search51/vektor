4f58d1cf54c0361ff30ab6e8c549bdfbc0dd7e28
Nils Honus <nils@honus.de>
2019-10-12 22:05:22 +0200

    irgendwelche Kommentare hingeschrieben (nicht vollständig und hab noch nicht drübergelesen)

--- ConsoleApplication1/ConsoleApplication1/Vektor.h
+++ ConsoleApplication1/ConsoleApplication1/Vektor.h
@@ -1,31 +1,40 @@
 #pragma once
+//template class with the datatype T
+//solution approach with linked lists
 template <class T>
 class Vektor
 {
-private:
+private:  
+  /*the element that will be createt and linked to another with the "next" and "last" pointer;
+  contains the content that will be stored inside a field of the vector,
+  as well as an identification numbre "id" to find the element*/
   struct element {
     struct element* last;
     struct element* next;
     T m_content;
     unsigned int id;
   };
+  //initializes pointer that mark the head and tail of the list by pointing on those elements
   struct element* head = 0;
   struct element* tail = 0;
+  //phelp is a helping pointer to search through the list
   struct element* phelp = 0;
+  //the size of the vector
   int m_size = 0;
 public:
 
   Vektor() {}
   ~Vektor() {}
 
-
+  //adds one element with a content value to the end of the list
   void pushback(T value) {
+    //if the list is empty
     if (head == 0) {
       head = new struct element;
       head->next = 0;
       head->last = 0;
       tail = head;
-    }
+    }    
     else {
       head->last = new struct element;
       head->last->last = 0;
@@ -36,6 +45,7 @@ public:
     head->id = m_size;
     ++m_size;
   }
+  //popback deletes the last element of the vector
   void popback() {
     if (head != 0) {
       if (m_size > 1) {
@@ -51,7 +61,7 @@ public:
       --m_size;
     }
   }
-
+  //at returns the value with the data typ T on the comitted position of the list
   T at(int pos) {
     if (m_size > 0) {
       phelp = tail;
@@ -65,10 +75,12 @@ public:
       }
     }
   }
-
+  //returns the size of the vector
   int size() {
     return m_size;
   }
+  /*resizes the vector to either shrinkining or enlarging it, which means the popback or pushback
+  function will be called repeatedly until the the size of the vector matches the intended size*/
   void resize(int size) {
     if (size < m_size) {
       do {
@@ -81,6 +93,7 @@ public:
       } while (size > m_size);
     }
   }
+  //clears the vector by poping back the elements until the vector is empty
   void clear() {
     while (head != 0) {
       popback();
